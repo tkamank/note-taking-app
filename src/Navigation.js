22e72886322a9ab4cb1b8f1e63704fbc4c11ca3f
Tariq <tkamankwah@captechu.edu>
2020-03-09 16:49:31 -0400

    Added .babelrc and dotenv

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,8 +8,14 @@ import Login from './screens/Login';
 import Home from './screens/Home';
 import HomeScreen from './screens/Home';
 const Stack = createStackNavigator();
-
-
+/*
+GoogleSignin.configure({
+    scopes: [], // what API you want to access on behalf of the user, default is email and profile
+    webClientId:  // client ID of type WEB for your server (needed to verify user ID and offline access)
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    iosClientId:  // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  });
+*/
 function Navigate(){
     return(
         <NavigationContainer>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -21,6 +21,23 @@ static navigationOptions = () => {
     }
 }
 
+_signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
 render(){
     return(
